8d2c15e13ea5bbfbe6734c050f68d3c83366cb43
conho-web <conho@internet.ru>
2022-06-09 18:05:24 +0300

    test Introsection Observer

--- pages/animations.js
+++ pages/animations.js
@@ -8,3 +8,40 @@ array.forEach(element => {
         opacity: 1,
     });
 });
+
+
+
+
+window.onload = () => {
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const lazyImg = entry.target
+                
+                gsap.to(lazyImg, {
+                    opacity: 1,
+                });
+
+                observer.unobserve(lazyImg)
+            }
+        })
+    }, options)
+
+
+    const arr = document.querySelectorAll("section");
+    const arr2 = document.querySelectorAll("img");
+
+    arr.forEach(i => {
+        observer.observe(i)
+    })
+
+    arr2.forEach(i => {
+        observer.observe(i)
+    })
+}
